f42be335bb2a733468fa1e2939305412b8d809af
Adam Ramberg <adam@mambojambostudios.com>
2023-08-29 19:38:37 +0200

    * Add support to mock slow request easily

--- app/api/tvmaze.ts
+++ app/api/tvmaze.ts
@@ -2,13 +2,19 @@ import { ShowResult } from "../types"
 
 const BASE_URL = "https://api.tvmaze.com"
 
+const sleep = async (time: number) => {
+  return new Promise((resolve) => setTimeout(resolve, time))
+}
+
 export const fetchShows = async (
   searchQuery: string | null | undefined,
+  sleepTime: number = 0,
 ): Promise<ShowResult[]> => {
   if (searchQuery == null) {
     return []
   }
   const result = await fetch(`${BASE_URL}/search/shows?q=${searchQuery}`)
+  await sleep(sleepTime)
   const json = await result.json()
   return json
 }
