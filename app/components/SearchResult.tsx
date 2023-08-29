ab77cc38e346361cc39303b1d8275d0dbd222752
Adam Ramberg <adam@mambojambostudios.com>
2023-08-29 14:51:07 +0200

    Create hook for async callbacks

--- app/components/SearchResult.tsx
+++ app/components/SearchResult.tsx
@@ -5,11 +5,6 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { Show } from "../types"
 import { fetchShows } from "../api/tvmaze"
 
-type Props = {
-  initialShows: Show[]
-  query: string | undefined
-}
-
 const useUpdateSearchParam = (key: string) => {
   const router = useRouter()
   const pathname = usePathname()
@@ -51,34 +46,85 @@ const useUpdateSearchParam = (key: string) => {
   return update
 }
 
+const SLOW_THRESHOLD = 1000
+
+const useStatefulAsyncCallback = <
+  R,
+  T extends (...params: any[]) => Promise<R>,
+>(
+  asyncCallback: T,
+  initialResult: R,
+  onSuccess: (() => void) | undefined = undefined,
+  onError: (() => void) | undefined = undefined,
+  slowThreshold = SLOW_THRESHOLD,
+): [
+  R,
+  (...params: Parameters<T>) => Promise<void>,
+  { isLoading: boolean; isSlow: boolean; error: undefined | Error },
+] => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState<Error | undefined>(undefined)
+  const [isSlow, setIsSlow] = useState(false)
+  const [result, setResult] = useState<R>(initialResult)
+
+  const runAsyncCallback = useCallback(
+    async (...params: Parameters<T>) => {
+      setError(undefined)
+      setIsSlow(false)
+      setIsLoading(true)
+
+      const timeout = setTimeout(() => {
+        setIsSlow(true)
+      }, slowThreshold)
+
+      try {
+        const res = await asyncCallback(...params)
+        setResult(res)
+        onSuccess?.()
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err)
+        }
+        onError?.()
+      } finally {
+        clearTimeout(timeout)
+        setIsLoading(false)
+      }
+    },
+    [asyncCallback, slowThreshold, onSuccess, onError],
+  )
+
+  return [result, runAsyncCallback, { isLoading, isSlow, error }]
+}
+
+type Props = {
+  initialShows: Show[]
+  query: string | undefined
+}
+
 export default function SearchResult({ initialShows = [], query = "" }: Props) {
-  const [shows, setShows] = useState<Show[]>(initialShows)
+  const [shows, updateShows, { isLoading, isSlow, error }] =
+    useStatefulAsyncCallback(fetchShows, initialShows)
   const updateSearchQuery = useUpdateSearchParam("q")
 
   useEffect(() => {
-    const refetchShows = async () => {
-      const wasUpdated = updateSearchQuery(query)
-      if (!wasUpdated) {
-        return
-      }
-      const result = await fetchShows(query)
-      setShows(result)
+    const wasUpdated = updateSearchQuery(query)
+    if (!wasUpdated) {
+      return
     }
-    refetchShows()
-  }, [query, updateSearchQuery])
+    updateShows(query)
+  }, [query, updateShows, updateSearchQuery])
 
   return (
-    <>
-      <div className="flex flex-col">
-        {shows.map(({ show }) => {
-          return (
-            <div key={show.id} className="flex flex-col items-center">
-              {/* {Boolean(show.image) && <Image src={show.image.medium} width="200" height="300" />} */}
-              <h1>{show.name}</h1>
-            </div>
-          )
-        })}
-      </div>
-    </>
+    <div className="flex flex-col">
+      {shows.map(({ show }) => {
+        return (
+          <div key={show.id} className="flex flex-col items-center">
+            {/* {Boolean(show.image) && <Image src={show.image.medium} width="200" height="300" />} */}
+            <h1>{show.name}</h1>
+          </div>
+        )
+      })}
+    </div>
   )
 }
